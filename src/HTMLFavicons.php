1946aea25f7dc53b948d0f4233389975a1396404
Jaimeson Waugh <jaimeson@4cesi.com>
2018-12-06 16:34:09 -0800

    Create basic version

--- src/HTMLFavicons.php
+++ src/HTMLFavicons.php
@@ -3,10 +3,39 @@
 declare( strict_types = 1 );
 namespace WaughJ\HTMLFavicons
 {
+	use WaughJ\FileLoader\FileLoader;
+
 	class HTMLFavicons
 	{
-		public function __construct()
+		public function __construct( string $website_name, FileLoader $loader = null )
 		{
+			$this->website_name = $website_name;
+			$this->loader = ( $loader === null ) ? new FileLoader() : $loader;
 		}
+
+		public function __toString()
+		{
+			return $this->getHTML();
+		}
+
+		public function getHTML() : string
+		{
+			return
+			"
+				<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"{$this->loader->getSourceWithVersion( 'apple-touch-icon.png' )}\">
+				<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"{$this->loader->getSourceWithVersion( 'favicon-32x32.png' )}\">
+				<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"{$this->loader->getSourceWithVersion( 'favicon-16x16.png' )}\">
+				<link rel=\"manifest\" href=\"{$this->loader->getSourceWithVersion( 'manifest.json' )}\">
+				<link rel=\"mask-icon\" href=\"{$this->loader->getSourceWithVersion( 'safari-pinned-tab.svg' )}\" color=\"#5bbad5\">
+				<link rel=\"shortcut icon\" href=\"{$this->loader->getSourceWithVersion( 'favicon.ico' )}\">
+				<meta name=\"apple-mobile-web-app-title\" content=\"{$this->website_name}\">
+				<meta name=\"application-name\" content=\"{$this->website_name}\">
+				<meta name=\"msapplication-config\" content=\"{$this->loader->getSourceWithVersion( 'browserconfig.xml' )}\">
+				<meta name=\"theme-color\" content=\"#ffffff\">
+			";
+		}
+
+		private $website_name;
+		private $loader;
 	}
 }
--- tests/HTMLFaviconsTest.php
+++ tests/HTMLFaviconsTest.php
@@ -2,12 +2,22 @@
 
 use PHPUnit\Framework\TestCase;
 use WaughJ\HTMLFavicons\HTMLFavicons;
+use WaughJ\FileLoader\FileLoader;
+use WaughJ\Directory\Directory;
 
 class HTMLFaviconsTest extends TestCase
 {
-	public function testObjectWorks()
+	public function testBasic()
 	{
-		$object = new HTMLFavicons();
-		$this->assertTrue( is_object( $object ) );
+		$favicons = new HTMLFavicons( "Favicon World" );
+		$this->assertContains( '<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">', $favicons->getHTML() );
+		$this->assertContains( '<meta name="application-name" content="Favicon World">', $favicons->getHTML() );
+	}
+
+	public function testWithLoader()
+	{
+		$favicons = new HTMLFavicons( "Favicon World", new FileLoader([ 'directory-url' => 'https://www.example.com/favicons', 'directory-server' => new Directory([ getcwd(), 'tests', 'favicons' ]) ]) );
+		$this->assertContains( '<link rel="apple-touch-icon" sizes="180x180" href="https://www.example.com/favicons/apple-touch-icon.png?m=', $favicons->getHTML() );
+		$this->assertContains( '<meta name="msapplication-config" content="https://www.example.com/favicons/browserconfig.xml?m=', $favicons->getHTML() );
 	}
 }
